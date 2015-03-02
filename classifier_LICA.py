d4a99f2ee2295ad7137b3ed9689eedb2ad8469d2
Matthew Ruttley <mruttley@mozilla.com>
2015-03-02 13:00:20 -0500

    Added domain/host/path rule functionality

--- classifier_LICA.py
+++ classifier_LICA.py
@@ -17,6 +17,44 @@ from re import findall
 from collections import defaultdict
 from tldextract import extract
 
+def make_tree(levels, end):
+	"""Recursively builds a tree.
+	`levels` are levels you want to integrate, e.g. ['one', 'two', 'three']
+	`end` is the value of the end item e.g. 'test'
+	The result would be: {'one': {'two': {'three': 'test'}}}
+	"""
+	if len(levels) == 1:
+		return {levels[0]: end}
+	else:
+		return {levels[0]: make_tree(levels[1:], end)}
+
+def check_tree(levels, tree):
+	"""Recursively checks a tree similar to the one made above in make_tree()"""
+	if len(levels) == 1:
+		if levels[0] in tree:
+			if type(tree[levels[0]]) != dict:
+				return tree[levels[0]]
+		return False
+	else:
+		if levels[0] in tree:
+			return check_tree(levels[1:], tree[levels[0]])
+		else:
+			return False
+
+def merge(a, b, path=None):
+    "merges b into a: http://stackoverflow.com/a/7205107/849354"
+    if path is None: path = []
+    for key in b:
+        if key in a:
+            if isinstance(a[key], dict) and isinstance(b[key], dict):
+                merge(a[key], b[key], path + [str(key)])
+            elif a[key] == b[key]:
+                pass # same leaf value
+            else:
+                raise Exception('Conflict at %s' % '.'.join(path + [str(key)]))
+        else:
+            a[key] = b[key]
+    return a
 
 class LICA:
 	"""Object that can classify a url using LICA."""
@@ -38,6 +76,30 @@ class LICA:
 		
 		#create a simple ignored words checker
 		self.ignored_words = set(self.payload["ignore_words"])
+		
+		#import the domain rules
+		with copen("payload_domain_rules.json", encoding='utf8') as f:
+			self.rules = load(f)
+		
+		#convert the host rules into an easily searchable format
+		# e.g. 		"au.movies.yahoo.com": "television",
+		# 			should be: "yahoo.com": { 'movies': { 'au': ['arts & entertainment', 'television'] } }
+		
+		self.host_rules = defaultdict(dict)
+		for host_rule, category in self.rules['host_rules'].iteritems():
+			domain = extract(host_rule) 				#ExtractResult(subdomain='au.movies', domain='yahoo', suffix='com')
+			tld = domain.domain + "." + domain.suffix 	# yahoo.com
+			host = domain.subdomain.split('.') 			#['au', 'movies']
+			tree = make_tree(host[::-1], category) 	#{ 'movies': { 'au': ['arts & entertainment', 'television'] } }
+			merge(self.host_rules, {tld: tree})	#merge the host rules with this new data
+		
+		#convert the path rules into an easily searchable format
+		self.path_rules = defaultdict(dict)
+		for path_rule, category in self.rules['path_rules'].iteritems():
+			domain = extract(path_rule) 
+			tld = domain.domain + "." + domain.suffix 	#sort of ignoring host+path rules, those can be covered by full DFR later
+			path = path_rule.split('/')[1]
+			self.path_rules[tld][path] = category
 	
 	def classify(self, url):
 		"""Returns a classification in the format [top_level, sub_level]
@@ -48,6 +110,27 @@ class LICA:
 		if tldinfo.domain in self.payload['ignore_domains']:
 			if tldinfo.suffix in self.payload['ignore_domains'][tldinfo.domain]:
 				return ['uncategorized', 'ignored', 1]
+		
+		#check if it is a single topic site
+		tld = tldinfo.domain + tldinfo.suffix
+		if tld in self.rules['domain_rules']:
+			return ['uncategorized', 'ignored', 1]
+		
+		#check if it is a single topic host
+		if tldinfo.subdomain:
+			if tld in self.host_rules:
+				domain_tree = self.host_rules[tld]
+				match = check_tree(tldinfo.subdomain.split('.'), domain_tree)
+				if match:
+					return match
+		
+		#check if it is a single topic path
+		if tld in self.path_rules:
+			path = url.split('/')
+			if len(path) > 1:
+				path = path[1]
+				if path in self.path_rules[tld]:
+					return self.path_rules[tld][path]
 		
 		#extract URL chunks
 		words = set(findall("[a-z]{3,}", url)) #extract 3+ character words from the url
@@ -94,12 +177,3 @@ class LICA:
 		sub_level = "/".join(sub_level[0][1])
 		
 		return [top_level, sub_level]
-
-
-
-
-
-
-
-
-
